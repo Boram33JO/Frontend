04278b44b4c5f812a2f37defd5edc137e45626ea
Aurora <eunbipower@gmail.com>
2023-07-31 16:43:36 +0900

    기본셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import EditorPage from "./pages/EditorPage";
+import DetailPage from "./pages/DetailPage";
+import ProfilePage from "./pages/ProfilePage";
+import MapPage from "./pages/MapPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={<MainPage />}
+                />
+                <Route
+                    path="/login"
+                    element={<LoginPage />}
+                />
+                <Route
+                    path="/signup"
+                    element={<SignupPage />}
+                />
+                <Route
+                    path="/editor"
+                    element={<EditorPage />}
+                />
+                <Route
+                    path="/detail/:id"
+                    element={<DetailPage />}
+                />
+                <Route
+                    path="/profile/:id"
+                    element={<ProfilePage />}
+                />
+                <Route
+                    path="/map"
+                    element={<MapPage />}
+                />
+                <Route
+                    path="/*"
+                    element={<NotFoundPage />}
+                />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
