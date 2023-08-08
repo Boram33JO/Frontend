4e63a9301da9b5ede8cb7df8fe0e9a105492a9c2
Aurora <eunbipower@gmail.com>
2023-08-08 09:31:34 +0900

    feat:지도 기본 폼

--- src/components/edit/EditMap.tsx
+++ src/components/edit/EditMap.tsx
@@ -8,39 +8,44 @@ declare global {
 
 const EditMap: React.FC = () => {
     const [state, setState] = useState({
-        center: { lat: 37.566826, lng: 126.9786567 },
+        center: { latitude: 37.566826, longitude: 126.9786567 },
         isPanto: true,
     });
+    // const [address, setAddress] = useState("");
     const [searchLocation, setSearchLocation] = useState<string>("");
-    const [markers, setMarkers] = useState<any[]>([]);
+    // const [markers, setMarkers] = useState<any[]>([]);
 
     useEffect(() => {
         const mapContainer = document.getElementById("map"); // 지도를 표시할 div
         const mapOption = {
-            center: new window.kakao.maps.LatLng(state.center.lat, state.center.lng), // 지도의 중심좌표
+            center: new window.kakao.maps.LatLng(state.center.latitude, state.center.longitude), // 지도의 중심좌표
             level: 3,
         };
 
         const map = new window.kakao.maps.Map(mapContainer, mapOption);
 
         if (state.isPanto) {
-            map.panTo(new window.kakao.maps.LatLng(state.center.lat, state.center.lng));
+            map.panTo(new window.kakao.maps.LatLng(state.center.latitude, state.center.longitude));
         } else {
-            map.setCenter(new window.kakao.maps.LatLng(state.center.lat, state.center.lng));
+            map.setCenter(new window.kakao.maps.LatLng(state.center.latitude, state.center.longitude));
         }
     }, [state]);
 
     const searchMap = () => {
         const ps = new window.kakao.maps.services.Places(); // 'window.' 추가
-        console.log(ps);
         const placesSearchCB = function (data: any, status: any, pagination: any) {
             if (status === window.kakao.maps.services.Status.OK) {
-                const newSearch = data;
-                console.log(newSearch);
-                setState({
-                    center: { lat: newSearch.y, lng: newSearch.x },
-                    isPanto: true,
-                });
+                
+                
+                
+                // const newSearch = data;
+                // console.log(newSearch);
+                // setAddress(newSearch.address_name);
+                // console.log("address", address);
+                // setState({
+                //     center: { latitude: newSearch.y, longitude: newSearch.x },
+                //     isPanto: true,
+                // });
             }
         };
         ps.keywordSearch(searchLocation, placesSearchCB); // 검색어 변경
@@ -50,7 +55,12 @@ const EditMap: React.FC = () => {
         setSearchLocation(event.target.value);
     };
 
-    const searchLocationHandler = () => {
+    const searchLocationHandler = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (searchLocation.trim().length === 0) {
+            return alert("내용을 입력하세요");
+        }
+        setSearchLocation("");
         searchMap();
     };
 
@@ -60,19 +70,13 @@ const EditMap: React.FC = () => {
                 id="map"
                 style={{ width: "350px", height: "288px" }}
             />
-            <input
-                onChange={changeInputHandler}
-                value={searchLocation}
-            />
-            <button onClick={searchLocationHandler}>검색</button>
-
-            <div>
-                <ul>
-                    <li>
-                        
-                    </li>
-                </ul>
-            </div>
+            <form onSubmit={searchLocationHandler}>
+                <input
+                    onChange={changeInputHandler}
+                    value={searchLocation}
+                />
+                <button>검색</button>
+            </form>
         </>
     );
 };
