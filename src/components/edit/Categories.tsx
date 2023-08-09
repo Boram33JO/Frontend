b8f4fd7ea56baf7859cd08b2c70e176ea4d9592d
Aurora <eunbipower@gmail.com>
2023-08-09 17:56:02 +0900

    Design:카테고리

--- src/components/edit/Categories.tsx
+++ src/components/edit/Categories.tsx
@@ -5,41 +5,53 @@ const Categories = () => {
     const clickCategoryHandler = (categoryId: any) => {
         console.log("categoryId", categoryId);
     };
-    
+
     const categories = ["카페", "식당", "대중교통", "학교", "운동", "공원", "물가", "바다", "도서관", "문화공간", "레저", "기타"];
 
     return (
-        <>
-            <CategoryLists>
+        <StScroll>
+            {/* <ScrollableComponent> */}
+            <StContainer>
                 {categories.map((category, index) => (
-                    <div
+                    <CategoryItem
                         key={index}
                         onClick={() => clickCategoryHandler(index + 1)}
                     >
                         {category}
-                    </div>
+                    </CategoryItem>
                 ))}
-            </CategoryLists>
-        </>
+            </StContainer>
+            {/* </ScrollableComponent> */}
+        </StScroll>
     );
 };
 
 export default Categories;
 
-const CategoryLists = styled.div`
-    overflow-y: hidden;
+const StScroll = styled.div`
     overflow-x: scroll;
-    /* height: 200px; */
-
-    div {
-        width: 60px;
-        height: 32px;
-        border: 1px solid black;
-        list-style-type: none;
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-        cursor: pointer;
+    overflow-y: hidden;
+    /* 스크롤바 숨기기 */
+    scrollbar-width: none; /* Firefox, IE, Edge */
+    &::-webkit-scrollbar {
+        width: 0 !important;
+        display: none;
     }
+`;
+
+const StContainer = styled.div`
+    display: flex;
+    padding: 20px;
+    gap: 20px;
+`;
+
+const CategoryItem = styled.div`
+    color: #fff;
+    background-color: #8084f3;
+    padding: 3px 10px;
+    border-radius: 20px;
+    white-space: nowrap; /* 가로로 출력 */
+
+    box-sizing: border-box;
+    cursor: pointer;
 `;
