f84cef40fdd128d248c2576637183228d3ef7771
heexohee <rsoy2918@naver.com>
2023-08-09 17:51:00 +0900

    Feat : 마이페이지의 하위 페이지 라우터 연결

---
+++ src/pages/MycommentsPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+
+
+
+const MycommentsPage = () => {
+  return (
+    <div>댓글단</div>
+  );
+};
+
+export default MycommentsPage;
