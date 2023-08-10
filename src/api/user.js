98a87cdae711e215ba060675c12b643577c8f2b3
heexohee <rsoy2918@naver.com>
2023-08-10 20:37:08 +0900

    Feat: 로컬스토리지에 추가 정보 담음

--- src/api/user.js
+++ src/api/user.js
@@ -19,7 +19,15 @@ const login = async (loginInformation) => {
   const response = await instance.post(`/api/user/login`, loginInformation);
   console.log("로그인", response);
   const token = response.headers.authorization;
+  const nickname = response.data.nickname;
+  const userImage = response.data.userImage;
+
   localStorage.setItem("token", token);
+  localStorage.setItem("nickname", nickname);
+  localStorage.setItem("userImage", userImage);
+
+  // console.log(response.data.nickname);
+  // console.log(response.data.userImage);
   return response.data;
 };
 
