b64bade67f789d51dff259968ff0fc3717f47f97
heexohee <rsoy2918@naver.com>
2023-08-11 22:16:45 +0900

    Design: 타인 계정 프로필 네비게이션 디자인

--- src/pages/MypostPage.tsx
+++ src/pages/MypostPage.tsx
@@ -4,14 +4,14 @@ import ProfileNav from "../components/profiledetail/ProfileNav";
 import GlobalStyle from "../components/common/GlobalStyle";
 import MyPostList from "../components/profiledetail/MyPostList";
 
-
-
+import ProfileNav2 from "../components/profiledetail/ProfileNav2";
 
 const MypostPage = () => {
     return (
       <>
       < GlobalStyle />
       < ProfileNav />
+      < ProfileNav2 />
       < MyPostList />
       
       <div>내가쓴</div>
