e4f33d22996c8b01242dd9415c2b353c09f0a228
Aurora <eunbipower@gmail.com>
2023-08-18 09:40:22 +0900

    Feat:html추가

--- src/components/login/KakaoLogin.tsx
+++ src/components/login/KakaoLogin.tsx
@@ -4,43 +4,43 @@ import { styled } from "styled-components";
 import { ReactComponent as KakaoSVG } from "../../assets/images/login_signup/kakao.svg"; // 변경된 부분
 
 const KakaoLogin = () => {
-  const REST_API_KEY = "545665f819304672fe24245d39231f28";
-  const REDIRECT_URI = `http://localhost:3000/api/oauth/token`;
-  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    const REST_API_KEY = "545665f819304672fe24245d39231f28";
+    const REDIRECT_URI = `http://localhost:3000/api/oauth/token`;
+    // const REDIRECT_URI = `  http://p.ple.s3-website.ap-northeast-2.amazonaws.com/api/oauth/token`;
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
-  // console.log(REST_API_KEY);
-  // console.log(REDIRECT_URI);
+    // console.log(REST_API_KEY);
+    // console.log(REDIRECT_URI);
 
-  const handleKakaoLogin = () => {
-    window.location.href = KAKAO_AUTH_URL;
-    // navigate(`/kakao/auth`)
-  };
+    const handleKakaoLogin = () => {
+        window.location.href = KAKAO_AUTH_URL;
+        // navigate(`/kakao/auth`)
+    };
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        cursor: "pointer"
-      }}
-    >
-      <KakaoSVG
-        className="kakaoLoginImage"
-        onClick={handleKakaoLogin} />
-    </div>
-  );
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                cursor: "pointer",
+            }}
+        >
+            <KakaoSVG
+                className="kakaoLoginImage"
+                onClick={handleKakaoLogin}
+            />
+        </div>
+    );
 };
 
 export default KakaoLogin;
 
-
 const Stlink2 = styled.a`
-  text-decoration: underline;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 500;
-  cursor: pointer;
-  color: #b2b2b2; 
+    text-decoration: underline;
+    font-size: 16px;
+    line-height: 24px;
+    font-weight: 500;
+    cursor: pointer;
+    color: #b2b2b2;
 `;
-
