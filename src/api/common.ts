bf9a1a9f0e587de8593def8579194874d11266ca
heexohee <rsoy2918@naver.com>
2023-08-21 13:24:57 +0900

    Fix : 엑세그 토큰 이슈 수정

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,5 +1,6 @@
-// 인스턴스
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 
 export const instance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
@@ -9,12 +10,12 @@ export const instance = axios.create({
 });
 
 instance.interceptors.request.use(
-  function (config) {
+  async function (config) {
     // 로컬 스토리지에서 토큰 값 가져오기
     const accessToken = localStorage.getItem("AccessToken");
     const refreshToken = localStorage.getItem("RefreshToken");
 
-    // 토큰이 존재하면 헤더에 담아서 요청 보내기
+    // 토큰이 존재하면 헤더에 담아서 요청 보내기 -> 둘 다 보내고 있었다.
     if (accessToken) {
       config.headers.AccessToken = `${accessToken}`;
       config.headers.RefreshToken = `${refreshToken}`;
@@ -22,6 +23,7 @@ instance.interceptors.request.use(
 
     return config;
   },
+
   function (error) {
     console.log("인터셉트 요청 오류!");
     return Promise.reject(error);
@@ -29,21 +31,45 @@ instance.interceptors.request.use(
 );
 
 instance.interceptors.response.use(
+  
   function (response) {
     // console.log("인터셉트 응답 받았어요!");
     // console.log("response", response);
     return response;
   },
-  function (error) {
-    console.log(error.response.data);
+  async function (error) {
+    //console.log(error.response.data);
     if (error.response.status === 400) {
       // const token = error.response.headers.authorization;
       // localStorage.setItem("token", token);
      
     }
     if (error.response.status === 401) {
-      localStorage.removeItem("token");
-      window.location.reload();
+     
+      //localStorage.removeItem();
+     // window.location.reload();
+      console.log("401 Unauthorized Error:", error);
+      console.log(error.response.data);
+      const newAccessToken = error.response.headers.accesstoken; // 새 엑세스 토큰 받아오기
+      if (newAccessToken) {
+        localStorage.removeItem("AccessToken");
+        localStorage.setItem("AccessToken", newAccessToken); // 새 엑세스 토큰 로컬 스토리지에 저장
+        // window.location.reload();
+        
+        // 지금은 리프레시가 같은 경우였고, 만약 리프레시까지 만료된 경우-> 로그아웃
+        // 엑세스를 안준다. 서버가 새것을 안준다.
+      } else {
+       //const navigate = useNavigate();
+        // 리프레시 토큰까지 만료되어 새 엑세스 토큰을 받을 수 없는 경우
+        localStorage.removeItem("AccessToken");
+        localStorage.removeItem("RefreshToken");
+        //여기까지 작동확인 됨.
+        localStorage.removeItem("pesist:root");
+        console.log('로그아웃 처리나 다른 조치를 수행하십시오.');
+        console.log(error.response.data);
+        //window.location.reload();
+       //navigate('/login');
+      }
     }
     return Promise.reject(error);
   }
