e36826ead5f311bd3951b44ad3f76fd3547f1d2c
Aurora <eunbipower@gmail.com>
2023-08-21 20:34:26 +0900

    Chore:api 주소 변경

--- src/api/edit.ts
+++ src/api/edit.ts
@@ -3,7 +3,7 @@ import instance from "./common";
 // 인기 리스트
 export const getPopularSongsList = async () => {
     try {
-        const response = await instance.get(`/api/AllMostSong`);
+        const response = await instance.get(`/api/song/AllMostSong`);
         // console.log("성공111122222", response);
         return response.data;
     } catch (error) {
@@ -14,7 +14,7 @@ export const getPopularSongsList = async () => {
 // 노래 검색
 export const getSearchSongs = async (searchSong: string) => {
     try {
-        const response = await instance.get(`/api/search?keyword=${searchSong}`);
+        const response = await instance.get(`/api/song/search?keyword=${searchSong}`);
         // console.log("성공", response);
         return response;
     } catch (error) {
