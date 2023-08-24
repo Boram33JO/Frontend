6fb071256e43b41cc4407e923eca91f97c43c99b
Aurora <eunbipower@gmail.com>
2023-08-24 14:28:52 +0900

    Fix:무한렌더링 해결

--- src/components/map/KakaoMap.tsx
+++ src/components/map/KakaoMap.tsx
@@ -1,20 +1,29 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { ReactComponent as SearchIcon } from "../../assets/images/search.svg";
-import Category from "../common/Category";
+import Categories from "../edit/Categories";
 import pinIcon from "../../assets/images/icon_pin_3x.png";
 import SearchModal from "../edit/SearchModal";
 
 import axios from "axios";
-import MyListItem from "../common/MyListItem";
-import { Post } from "../../models/post";
+import { postData } from "../../api/map";
 
-//setAddress: (address: string) => void;
-// setPlaceName: (placeName: string) => void;
+interface Location {
+    placeName: string;
+    latitude: string;
+    longitude: string;
+}
 
-const KakaoMap: React.FC = () => {
-    const [latitude, setLatitude] = useState<string>("37.566826");
-    const [longitude, setLongitude] = useState<string>("126.9786567");
+interface KakaoProps {
+    isData: Location[]; // 데이터 구조에 따라 변경
+    setIsData: React.Dispatch<React.SetStateAction<Location[]>>; // 데이터 구조에 따라 변경
+    latitude: string;
+    setLatitude: (latitude: string) => void;
+    longitude: string;
+    setLongitude: (longitude: string) => void;
+}
+
+const KakaoMap: React.FC<KakaoProps> = ({ isData, setIsData, latitude, setLatitude, longitude, setLongitude }) => {
     const [searchLocation, setSearchLocation] = useState<string>("");
     const [searchLocationList, setSearchLocationList] = useState<any>([]);
     const [address, setAddress] = useState("");
@@ -22,90 +31,84 @@ const KakaoMap: React.FC = () => {
     const [selectedLocation, setSelectedLocation] = useState<any>({});
     const [categoryNum, setCategoryNum] = useState<number>(0);
     const [modal, setModal] = useState(false);
-    const [positions, setPositions] = useState<any>([]);
-    const [isData, setIsData] = useState<any>([]);
-    //     {
-    //         title: "스타벅스 강남R점",
-    //         latlng: new window.kakao.maps.LatLng(37.4976744709989, 127.028443419181),
-    //     },
-    //     {
-    //         title: "스타벅스 청담스타R점",
-    //         latlng: new window.kakao.maps.LatLng(37.5252051860766, 127.041816506596),
-    //     },
-    //     {
-    //         title: "스타벅스 청담사거리점",
-    //         latlng: new window.kakao.maps.LatLng(37.523735555011335, 127.046872393057),
-    //     },
-    //     {
-    //         title: "스타벅스 수서역R점",
-    //         latlng: new window.kakao.maps.LatLng(37.48800665367514, 127.10297988971773),
-    //     },
-    //     {
-    //         title: "스타벅스 신사역점",
-    //         latlng: new window.kakao.maps.LatLng(37.51622596162784, 127.0207677490634),
-    //     },
-    //     {
-    //         title: "스타벅스 스타필드코엑스몰 R점",
-    //         latlng: new window.kakao.maps.LatLng(37.50979926297963, 127.06163058645485),
-    //     },
-    //     {
-    //         title: "스타벅스 강남대로점",
-    //         latlng: new window.kakao.maps.LatLng(37.5032649716005, 127.025559367137),
-    //     },
-    //     {
-    //         title: "스타벅스 압구정로데오역점",
-    //         latlng: new window.kakao.maps.LatLng(37.5265751539424, 127.040541168059),
-    //     },
-    //     {
-    //         title: "스타벅스 선릉역점",
-    //         latlng: new window.kakao.maps.LatLng(37.5038956552172, 127.04859034788),
-    //     },
 
-    //     {
-    //         title: "스타벅스 양재강남빌딩R점",
-    //         latlng: new window.kakao.maps.LatLng(37.48516628428305, 127.03646645149259),
-    //     },
-    //     {
-    //         title: "스타벅스 선릉세화빌딩점",
-    //         latlng: new window.kakao.maps.LatLng(37.5037864222678, 127.051223136165),
-    //     },
-    //     {
-    //         title: "스타벅스 강남비젼타워점",
-    //         latlng: new window.kakao.maps.LatLng(37.49649026006633, 127.0296167853337),
-    //     },
-    //     {
-    //         title: "스타벅스 강남구청역점",
-    //         latlng: new window.kakao.maps.LatLng(37.5167215373803, 127.041264296462),
-    //     },
-    //     {
-    //         title: "스타벅스 청담점",
-    //         latlng: new window.kakao.maps.LatLng(37.5243543810765, 127.051585624477),
-    //     },
-    //     {
-    //         title: "스타벅스 포이점",
-    //         latlng: new window.kakao.maps.LatLng(37.4778218663276, 127.045145597366),
-    //     },
-    // ]);
+    const [selectedMarker, setSelectedMarker] = useState(null);
 
     const addMarkersToMap = (map: any, positions: any[]) => {
         const imageSrc = pinIcon;
-
         for (let i = 0; i < positions.length; i++) {
             const imageSize = new window.kakao.maps.Size(36, 42);
             const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize);
 
             const marker = new window.kakao.maps.Marker({
                 map: map,
+                // position: new window.kakao.maps.LatLng(isData[i]?.location?.latitude, isData[i]?.location?.longitude),
                 position: positions[i].latlng,
+                // title: isData[i]?.location?.placeName,
                 title: positions[i].title,
                 image: markerImage,
             });
+            // 마커가 지도 위에 표시되도록 설정합니다
+            marker.setMap(map);
+        }
+    };
+
+    // 최초 GPS 위치 설정
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                function (position) {
+                    const lat = String(position.coords.latitude);
+                    const lng = String(position.coords.longitude);
+
+                    setLatitude(lat);
+                    setLongitude(lng);
+                },
+                (error) => {
+                    console.error("error", error);
+                }
+            );
+        } else {
+            console.error("해당 브라우저에서는 gps를 지원하지 않습니다.");
+        }
+    }, []);
+
+    // 마커 데이터 업데이트
+    useEffect(() => {
+        mappingList().then((res) => {
+            window.kakao.maps.load(() => {
+                const mapContainer = document.getElementById("map");
+                const map = new window.kakao.maps.Map(mapContainer, {
+                    center: new window.kakao.maps.LatLng(latitude, longitude),
+                    level: 3,
+                });
+                // 좌표로 이동
+                map.panTo(new window.kakao.maps.LatLng(latitude, longitude));
+                // 좌표에 있는 data로 markers에 넣을 배열 생성
+                const position = res.map((item: any) => ({
+                    title: item.location.placeName,
+                    latlng: new window.kakao.maps.LatLng(item.location.latitude, item.location.longitude),
+                }));
+                // 마커 추가 로직 호출
+                addMarkersToMap(map, position);
+            });
+        });
+    }, [latitude, longitude]);
+
+    const mappingList = async () => {
+        const latlng = { latitude, longitude };
+        try {
+            const response = await postData(latlng);
+            setIsData(response?.data.content);
+            return response?.data.content;
+        } catch (error) {
+            console.error(error);
         }
     };
 
     const searchMap = () => {
         const ps = new window.kakao.maps.services.Places();
-        const placesSearchCB = function (data: any, status: any, pagination: any) {
+        const placesSearchCB = function (data: any, status: any) {
             if (status === window.kakao.maps.services.Status.OK) {
                 setSearchLocationList(data);
                 setLatitude(selectedLocation.y); // 검색한 위치의 위도로 변경
@@ -115,66 +118,7 @@ const KakaoMap: React.FC = () => {
         ps.keywordSearch(searchLocation, placesSearchCB);
     };
 
-    console.log("이거 맞음", latitude, longitude);
-
-    // 로딩후 최초 1회 실행, 그때 GPS 받은 상태면 위도/경도를 GPS 위치로 설정
-    useEffect(() => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                console.log("네비게이션");
-                const lat = String(position.coords.latitude);
-                const lng = String(position.coords.longitude);
-
-                setLatitude(lat);
-                setLongitude(lng);
-            });
-        }
-        // mappingList();
-    }, []);
-
-    const mappingList = async () => {
-        try {
-            const response = await axios.post(`https://api.pple.today/api/posts/map?page=0&size=4`, {
-                latitude,
-                longitude,
-            });
-            setIsData(response.data.content);
-            console.log("dididi", isData); // 업데이트 된 상태를 찍어보기
-            console.log("성공", response);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    console.log("isData", isData);
-    console.log("length", Object.keys(isData).length);
-
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_REST_API_KEY}&libraries=services&autoload=false`;
-        document.head.appendChild(script);
-        // console.log("검색어", Boolean(isSearch), isSearch);
-        script.onload = () => {
-            window.kakao.maps.load(() => {
-                const mapContainer = document.getElementById("map");
-                const mapOption = {
-                    center: new window.kakao.maps.LatLng(latitude, longitude),
-                    level: 3,
-                };
-                const map = new window.kakao.maps.Map(mapContainer, mapOption);
-
-                map.panTo(new window.kakao.maps.LatLng(latitude, longitude));
-
-                // searchMap();
-                addMarkersToMap(map, positions);
-            });
-        };
-        mappingList();
-    }, [latitude, longitude]);
-
-    console.log("bbdd", selectedLocation);
-    console.log("11", latitude);
-    console.log("22", longitude);
+    // console.log("이거 맞음", latitude, longitude);
 
     const changeInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchLocation(event.target.value);
@@ -186,13 +130,13 @@ const KakaoMap: React.FC = () => {
             return alert("내용을 입력하세요");
         }
         setModal(true);
-        // setIsSearch(true);
         setSearchLocation("");
         searchMap();
     };
 
     return (
         <>
+            <script></script>
             <StMapContainer>
                 <StSearchForm onSubmit={searchLocationHandler}>
                     <div>
@@ -224,26 +168,13 @@ const KakaoMap: React.FC = () => {
                     />
                 )}
                 <StCategory>
-                    <Category
+                    <Categories
                         categoryNum={categoryNum}
                         setCategoryNum={setCategoryNum}
                     />
                 </StCategory>
                 <StKakaoMap id="map" />
             </StMapContainer>
-            {/* <StLine /> */}
-            <StListContainer>
-                {isData.length === 0 ? (
-                    <div>해당 위치에 포스팅이 없습니다. 다른 지역을 검색해보세요.</div>
-                ) : (
-                    isData.map((post: Post) => (
-                        <MyListItem
-                            key={post.postId}
-                            post={post}
-                        />
-                    ))
-                )}
-            </StListContainer>
         </>
     );
 };
@@ -285,16 +216,123 @@ const StKakaoMap = styled.div`
     border-radius: 10px;
 `;
 
-// const StLine = styled.div`
-//     width: 390px;
-//     height: 8px;
-//     background: #242325;
-// `;
+// // positions 배열을 매핑하고 각 객체를 위치 정보로 업데이트합니다
+// const updatedPositions = positions.map((position, index) => {
+//     const locationInfo = response.locations[index]; // 해당 위치 정보 가져오기
+//     if (locationInfo) {
+//         const { latitude, longitude, placeName } = locationInfo;
+//         return {
+//             ...position,
+//             latlng: new window.kakao.maps.LatLng(latitude, longitude), // LatLng 객체를 사용한다고 가정합니다
+//             title: placeName,
+//         };
+//     }
+//     return position; // 해당하는 위치 정보가 없으면 원래 객체를 그대로 유지합니다
+// });
 
-const StListContainer = styled.div`
-    width: 350px;
-    height: 523px;
-    /* padding: 0 20px; */
-    background-color: beige;
-    /* background-color: #141414; */
-`;
+// // 이제 updatedPositions는 업데이트된 위치 정보가 포함된 새 배열입니다
+// setPositions(updatedPositions);
+// setPositions()
+
+//     {
+//         title: "스타벅스 청담스타R점",
+//         latlng: new window.kakao.maps.LatLng(37.5252051860766, 127.041816506596),
+//     },
+//     {
+//         title: "스타벅스 청담사거리점",
+//         latlng: new window.kakao.maps.LatLng(37.523735555011335, 127.046872393057),
+//     },
+//     {
+//         title: "스타벅스 수서역R점",
+//         latlng: new window.kakao.maps.LatLng(37.48800665367514, 127.10297988971773),
+//     },
+//     {
+//         title: "스타벅스 신사역점",
+//         latlng: new window.kakao.maps.LatLng(37.51622596162784, 127.0207677490634),
+//     },
+//     {
+//         title: "스타벅스 스타필드코엑스몰 R점",
+//         latlng: new window.kakao.maps.LatLng(37.50979926297963, 127.06163058645485),
+//     },
+//     {
+//         title: "스타벅스 강남대로점",
+//         latlng: new window.kakao.maps.LatLng(37.5032649716005, 127.025559367137),
+//     },
+//     {
+//         title: "스타벅스 압구정로데오역점",
+//         latlng: new window.kakao.maps.LatLng(37.5265751539424, 127.040541168059),
+//     },
+//     {
+//         title: "스타벅스 선릉역점",
+//         latlng: new window.kakao.maps.LatLng(37.5038956552172, 127.04859034788),
+//     },
+
+//     {
+//         title: "스타벅스 양재강남빌딩R점",
+//         latlng: new window.kakao.maps.LatLng(37.48516628428305, 127.03646645149259),
+//     },
+//     {
+//         title: "스타벅스 선릉세화빌딩점",
+//         latlng: new window.kakao.maps.LatLng(37.5037864222678, 127.051223136165),
+//     },
+//     {
+//         title: "스타벅스 강남비젼타워점",
+//         latlng: new window.kakao.maps.LatLng(37.49649026006633, 127.0296167853337),
+//     },
+//     {
+//         title: "스타벅스 강남구청역점",
+//         latlng: new window.kakao.maps.LatLng(37.5167215373803, 127.041264296462),
+//     },
+//     {
+//         title: "스타벅스 청담점",
+//         latlng: new window.kakao.maps.LatLng(37.5243543810765, 127.051585624477),
+//     },
+//     {
+//         title: "스타벅스 포이점",
+//         latlng: new window.kakao.maps.LatLng(37.4778218663276, 127.045145597366),
+//     },
+// ]);
+
+// const addMarkersToMap = (map: any, positions: any[]) => {
+//     const imageSrc = pinIcon;
+
+//     for (let i = 0; i < positions.length; i++) {
+//         const imageSize = new window.kakao.maps.Size(36, 42);
+//         const markerImage = new window.kakao.maps.MarkerImage(imageSrc, imageSize);
+
+//         const marker = new window.kakao.maps.Marker({
+//             map: map,
+//                         position: new window.kakao.maps.LatLng(positions[i].latitude, positions[i].longitude),
+
+//             title: positions[i].title,
+//             image: markerImage,
+//         });
+//     }
+//     positions.forEach((el) => {
+//         // 마커를 생성합니다
+//         new window.kakao.maps.Marker({
+//           //마커가 표시 될 지도
+//           map: map,
+//           //마커가 표시 될 위치
+//           position: new window.kakao.maps.LatLng(el.lat, el.lng),
+//           //마커에 hover시 나타날 title
+//           title: el.title,
+//         });
+//       });
+//     };
+
+// };
+
+// const addMarkersToMap = (map: any, positions: any[]) => {
+//     positions.forEach((el) => {
+//         // el.latlng에 있는 위도와 경도 정보를 가져와서 마커를 생성합니다
+//         new window.kakao.maps.Marker({
+//             // 마커가 표시될 지도
+//             map: map,
+//             // 마커가 표시될 위치
+//             position: el.latlng,
+//             // 마커에 hover 시 나타날 title
+//             title: el.title,
+//         });
+//     });
+// };
--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -2,16 +2,43 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 import KakaoMap from "../components/map/KakaoMap";
-// import MyListItem from "../components/common/MyListItem";
-// import { Post } from '../models/post';
+import MyListItem from "../components/common/MyListItem";
+import { Post } from "../models/post";
 
 const MapPage = () => {
+    const [isData, setIsData] = useState<any>([]);
+    const [latitude, setLatitude] = useState<string>("37.566826");
+    const [longitude, setLongitude] = useState<string>("126.9786567");
+
     return (
         <>
             <StMapContainer>
                 <h1>지금 피플러는 뭘 듣고 있을까요?</h1>
-                <KakaoMap />
+                <KakaoMap
+                    isData={isData}
+                    setIsData={setIsData}
+                    latitude={latitude}
+                    setLatitude={setLatitude}
+                    longitude={longitude}
+                    setLongitude={setLongitude}
+                />
             </StMapContainer>
+            <StLine />
+            <StListContainer>
+                <h1>{isData.length}의 포스팅 결과</h1>
+                {isData.length === 0 ? (
+                    <div>해당 위치에 포스팅이 없습니다. 다른 지역을 검색해보세요.</div>
+                ) : (
+                    isData.map((post: Post) => (
+                        <StMyListItem>
+                            <MyListItem
+                                key={post.postId}
+                                post={post}
+                            />
+                        </StMyListItem>
+                    ))
+                )}
+            </StListContainer>
         </>
     );
 };
@@ -23,12 +50,34 @@ export const StMapContainer = styled.div`
     /* height: 523px; */
     padding: 0 20px;
     margin-top: 47px;
-    background-color: beige;
-    /* background-color: #141414; */
+    background-color: #141414;
 
     h1 {
         font-size: 20px;
         color: #fafafa;
         margin-bottom: 16px;
     }
+`;
+
+const StLine = styled.div`
+    width: 390px;
+    height: 8px;
+    background: #242325;
+`;
+
+const StListContainer = styled.div`
+    width: 350px;
+    /* height: 523px; */
+    padding: 0 20px 78px 20px;
+    background-color: #141414;
+    color: #fafafa;
+
+    h1 {
+        font-size: 20px;
+        padding: 31px 0 16px 0;
+    }
+`;
+
+const StMyListItem = styled.div`
+    margin-bottom: 14px;
 `;
