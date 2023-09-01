7645562ec2f96bad077fa25a8e0e91ca2094fa02
Aurora <eunbipower@gmail.com>
2023-09-01 09:20:39 +0900

    Feat:짇도 리스트 페이지네이션

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -6,12 +6,22 @@ import { Post } from "../models/post";
 import ListItem from "../components/common/ListItem";
 import Loading from "../components/map/Loading";
 
+import { ReactComponent as Start } from "../assets/images/page_start.svg";
+import { ReactComponent as End } from "../assets/images/page_end.svg";
+import { ReactComponent as Prev } from "../assets/images/page_prev.svg";
+import { ReactComponent as Next } from "../assets/images/page_next.svg";
+
 const MapPage = () => {
     const [postList, setPostList] = useState<any>([]);
     const [isData, setIsData] = useState<any>([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [loading, setLoading] = useState<boolean>(false);
+    // const [page, setPage] = useState<number>(0);
+    // const [totalPage, setTotalPage] = useState<number>(0);
+    const [startPage, setStartPage] = useState(1);
+    const pageLimit = 5; // 한 번에 보여줄 페이지 번호 수
     const perPage = 10; // 한 페이지에 보여줄 아이템 수
+    const totalPage = Math.ceil(postList.length / perPage);
 
     useEffect(() => {
         setLoading(true);
@@ -23,12 +33,37 @@ const MapPage = () => {
         return () => clearTimeout(timer);
     }, []);
 
-    // 페이지 버튼 클릭 시 호출될 함수
-    const handlePageClick = (pageNumber: number) => {
+    const FirstClickHandler = () => {
+        setCurrentPage(1);
+        setStartPage(1);
+    };
+    const PrevClickHandler = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+            if (startPage > 1) {
+                setStartPage(startPage - 1);
+            }
+        }
+    };
+    const NextClickHandler = () => {
+        if (currentPage < totalPage) {
+            setCurrentPage(currentPage + 1);
+            if (currentPage % pageLimit === 0) {
+                // If we're at the end of the current page range
+                setStartPage(startPage + pageLimit); // Move to the next range of pages
+            }
+        }
+    };
+
+    const LastClickHandler = () => {
+        const lastPageIndex = Math.ceil(postList.length / perPage);
+        setCurrentPage(lastPageIndex);
+    };
+
+    const CurrentClickHandler = (pageNumber: number) => {
         setCurrentPage(pageNumber);
     };
 
-    // 현재 페이지에 해당하는 포스트만 필터링
     const currentPosts = postList.slice((currentPage - 1) * perPage, currentPage * perPage);
 
     return (
@@ -62,16 +97,29 @@ const MapPage = () => {
                     })
                 )}
                 {/* Pagination */}
-                <StButtonContainer>
-                    {[...Array(Math.ceil(postList.length / perPage))].map((_, index) => (
-                        <StPagenationButton
-                            key={index + 1}
-                            onClick={() => handlePageClick(index + 1)}
+                <CommentListPagination>
+                    <SvgIcon>
+                        <Start onClick={FirstClickHandler} />
+                    </SvgIcon>
+                    <SvgIcon>
+                        <Prev onClick={PrevClickHandler} />
+                    </SvgIcon>
+                    {[...Array(Math.min(pageLimit, Math.ceil(postList.length / perPage)))].map((_, index) => (
+                        <PageButton
+                            key={index}
+                            $click={currentPage === startPage + index}
+                            onClick={() => CurrentClickHandler(startPage + index)}
                         >
-                            {index + 1}
-                        </StPagenationButton>
+                            {startPage + index}
+                        </PageButton>
                     ))}
-                </StButtonContainer>
+                    <SvgIcon>
+                        <Next onClick={NextClickHandler} />
+                    </SvgIcon>
+                    <SvgIcon>
+                        <End onClick={LastClickHandler} />
+                    </SvgIcon>
+                </CommentListPagination>
             </StListContainer>
         </InnerContainer>
     );
@@ -122,13 +170,37 @@ const StMyListItem = styled.div`
     margin-bottom: 14px;
 `;
 
-const StButtonContainer = styled.div`
+const CommentListPagination = styled.div`
     display: flex;
     justify-content: center;
+    align-items: center;
+
+    box-sizing: border-box;
+    margin: 40px 0px 10px;
+    gap: 10px;
 `;
 
-const StPagenationButton = styled.button`
-    color: #fafafa;
-    margin: 10px;
+const SvgIcon = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 30px;
+    height: 30px;
+    cursor: pointer;
+`;
+
+const Pagination = styled.div`
+    display: flex;
+`;
+
+const PageButton = styled.div<{ $click: boolean }>`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 36px;
+    height: 36px;
+    border-radius: 100%;
+    background-color: ${(props) => (props.$click ? "#7462E2" : "transparent")};
+    color: ${(props) => (props.$click ? "#FAFAFA" : "#535258")};
     cursor: pointer;
 `;
