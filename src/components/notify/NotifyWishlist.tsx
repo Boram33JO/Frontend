0f0d4c2c3baad572c68a936925f49262460c174b
DESKTOP-8GS2VKA\ownage <kjw2878@gmail.com>
2023-09-07 11:07:22 +0900

    Feat: update notify

--- src/components/notify/NotifyWishlist.tsx
+++ src/components/notify/NotifyWishlist.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { styled } from 'styled-components'
 import NotifyItem from './NotifyItem'
+import NotifyEmpty from './NotifyEmpty';
 
 interface Props {
     data: any;
@@ -19,7 +20,7 @@ const NotifyWishlist = ({ data }: Props) => {
                 </P>
             </TitleSection>
             <NotifyList>
-                {
+                {(data === undefined || data?.length === 0) ? <NotifyEmpty type="좋아요" /> : (
                     data?.map((item: any) => {
                         return (
                             <NotifyItem
@@ -36,7 +37,7 @@ const NotifyWishlist = ({ data }: Props) => {
                             />
                         )
                     })
-                }
+                )}
             </NotifyList>
         </Container>
     )
